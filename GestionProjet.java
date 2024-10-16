1cf6ef416c9ed847e552dd2c7ef67f9f73c7ef32
Rodrigue Tiendrebeogo <tiendrebeogorodrigue104@gmail.com>
2024-10-16 22:03:45 +0200

    init

---
+++ GestionProjet.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.gestionprojet;
+
+/**
+ *
+ * @author user
+ */
+public class GestionProjet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
